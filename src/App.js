64ea1cfc68b0410c3dc989e9073e1c57fd177b0a
nonsodaniel <nonsodaniel07@gmail.com>
2019-06-04 20:21:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// import Navbar from "./components/layouts/Navbar";
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
