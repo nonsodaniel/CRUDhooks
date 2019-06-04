787845d2dcfeacca60c63555eb5f8816c7112fd1
nonsodaniel <nonsodaniel07@gmail.com>
2019-06-04 20:30:28 +0100

    Creating a view for the app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from "react";
 
 // import Navbar from "./components/layouts/Navbar";
 
-class App extends Component {
-  state = {};
-
-  render() {
-    return (
-      <React.Fragment>
-
-      </React.Fragment>
-    );
-  }
+const App = () => {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <h1>CRUD App with Hooks</h1>
+        <div className="flex-row">
+          <div className="flex-large">
+            <h2>Add user</h2>
+          </div>
+          <div className="flex-large">
+            <h2>View users</h2>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default App;
